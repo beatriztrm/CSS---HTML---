84aad878fb7734b03bca51dfbaadf67027aa5e1b
beatriztrm <beatriztrudes98@gmail.com>
2026-03-17 22:22:37 -0300

    vendas

--- React/olamundo/app/vendas/page.js
+++ React/olamundo/app/vendas/page.js
@@ -6,39 +6,90 @@ supabase
 
 function Vendas() {
 
+    const [usuario, alteraUsuario] = useState()
+    const [livro, alteraLivro] = useState()
+    const [quantidade, alteraQuantidade] = useState()
+    const [pagamento, alteraPagamento] = useState()
 
     const [listaVendas, alteraListaVendas] = useState([])
 
     async function buscaTodos() {
+
+
+
+
         const { data, error } = await supabase
             .from('vendas')
             .select(`
                 *,
-                id_usuario(*)
+                id_usuario(*),
                 id_livro(*)
                 `)
-                
-                console.log(data)
-                
-                    
-                
+
+        console.log(data)
+
+
+
 
         alteraListaVendas(data)
     }
 
-    function formataData(data){
+    function formataData(data) {
         let data_Formatada = new Date(data)
         data_Formatada = data_Formatada.toLocaleDateString()
         return data_Formatada
 
     }
 
-    function formataHoras(horas){
+    function formataHoras(horas) {
         let horas_Formatadas = new Date(horas)
-        horas_Formatadas = horas_Formatadas.toLocaleDateString()
+        horas_Formatadas = horas_Formatadas.toLocaleTimeString()
         return horas_Formatadas
 
     }
+
+    function formataPagamento(pagamento) {
+
+        if (pagamento == "Pix") {
+            return <span class="badge bg-success">PIX</span>
+        }
+
+
+        if (pagamento == "Cartão de Crédito") {
+            return <span class="badge bg-danger">Cartão de Crédito</span>
+        }
+
+
+        if (pagamento == "Boleto") {
+            return <span class="badge bg-secondary">Boleto</span>
+        }
+
+
+        if (pagamento == "Cartão de Débito") {
+            return <span class="badge bg-danger">Cartão de Débito</span>
+        }
+
+        if (pagamento == "Dinheiro") {
+            return <span class="badge bg-warning text-dark">Dinheiro</span>
+        }
+
+
+    }
+    async function salvar(e) {
+        e.preventDefault()
+
+        const objeto = {
+            id_usuario: usuario,
+            id_livro: livro,
+            quantidade: quantidade,
+            pagamento: pagamento,
+        }
+        console.log(objeto)
+
+        const { error } = await supabase.from('vendas').insert(objeto)
+        console.log(error)
+    }
+
 
 
 
@@ -47,19 +98,46 @@ function Vendas() {
     }, [])
 
 
-
-
-
-
-
     return (
         <div>
 
             <h1>Vendas</h1>
             <hr />
 
+            <form onSubmit={salvar}>
+                <p>Selecione o usuário</p>
+                <select onChange={e => alteraUsuario(e.target.value)}>
+                    {
+                        listaVendas.map(
+                            item => <option value={item.id_usuario.id}>{item.id_usuario.nome}</option>
+                        )
+                    }
+                </select>
+                <p>Selecione o Livro</p>
+
+                <select onChange={e => alteraLivro(e.target.value)}>
+                    {
+                        listaVendas.map(
+                            item => <option value={item.id_livro.id}>{item.id_livro.titulo}</option>
+                        )
+                    }
+                </select>
+
+                <p>Digite a quantidade</p>
+                <input onChange={e => alteraQuantidade(e.target.value)} />
+                <p>Digite o pagamento</p>
+                <input onChange={e => alteraPagamento(e.target.value)} />
+                <br></br>
+                <br></br>
+                <button>Salvar</button>
+
+            </form>
+
+            <hr></hr>
+
             <table class="table">
                 <tr>
+                    <td>#</td>
                     <td>Nome</td>
                     <td>Livro</td>
                     <td>Quantidade</td>
@@ -72,12 +150,13 @@ function Vendas() {
                         <p>Carregando..</p>
                         :
                         listaVendas.map(
-                            item => <tr>
+                            (item, index) => <tr>
+                                <td>{index}</td>
                                 <td> {item.id_usuario.nome} </td>
-                                <td> {item.id_livro.nome} </td>
+                                <td> {item.id_livro.titulo} </td>
                                 <td> {item.quantidade}</td>
-                                <td>{item.pagamento}</td>
-                                <td>{item.created_at}</td>
+                                <td>{formataPagamento(item.pagamento)}</td>
+                                <td>{formataData(item.created_at)} às {formataHoras(item.created_at)}</td>
                             </tr>
                         )
                 }
