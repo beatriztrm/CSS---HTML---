fd42d551b91fae0caa1022097e93792b7aadcf85
beatriztrm <beatriztrudes98@gmail.com>
2026-03-18 21:54:45 -0300

    att

--- React/olamundo/app/vendas/[id]/page.js
+++ React/olamundo/app/vendas/[id]/page.js
@@ -1,26 +1,54 @@
 'use client'
 import supabase from "@/app/conexao/supabase";
 import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
-async function ConsultaVendas() {
+function ConsultaVendas() {
 
 
     const params = useParams()
 
-    const { data, error } = await supabase
-        .from('vendas')
-        .select()
-        .eq('id', params.id)
+    const [listaVendas, alteraListaVendas] = useState([])
+
+    async function buscaVendas() {
+
+        const { data, error } = await supabase
+            .from('vendas')
+            .select(`
+                *,
+                id_usuario(*),
+                id_livro(*)
+                `)
+            .eq('id', params.id)
+
+        alteraListaVendas(data)
+    }
+
+    useEffect(() => {
+        buscaVendas()
+    }, [])
+
 
     return (
         <div>
             <h1>Consulta de vendas</h1>
             <hr></hr>
-            <p>ID da venda é: {params.id}</p>
-            <p>comprador:  {params.usuario}</p>
-            <p>Produto: {params.livro}</p>
-            <p>Quantidade: {params.quantidade}</p>
-            <p>Data da Compra: {params.created.id}</p>
+
+            {listaVendas.map(
+                item =>
+                <div>
+
+                    <p>ID da venda é: {item.id}</p>
+                    <p>Comprador:  {item.id_usuario.nome}</p>
+                    <p>Produto: {item.id_livro.titulo}</p>
+                    <p>Quantidade: {item.quantidade}</p>
+                    <p>Data da Compra: {item.created_at}</p>
+
+
+                </div>
+            )}
+
+
 
         </div>
 
